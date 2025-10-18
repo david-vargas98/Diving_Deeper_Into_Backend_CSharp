ed1f0b8c84f29a3776611a01f6977bee97cd57c5
david-vargas98 <edgar981125@live.com.mx>
2025-10-18 09:40:35 -0600

    feat: basic types.

--- CsBases/Program.cs
+++ CsBases/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        // explicit declaration
+        int quantity = 3;
+        string message = "Hello world!";
+        decimal price = 19.99m;
+
+        WriteLine($"Quantity: {quantity}, Message: {message}, Price: {price:C}");
+
+        // var declaration: it'll infer the type
+        var greeting = "hey";
+        var percentage = 20.00m;
+
+        WriteLine($"Greeting: {greeting}, Percentage: {percentage}");
+    }
+}
